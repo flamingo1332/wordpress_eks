bdaf799cd5d86b1e57bf1f2faf5ddf10a5bfad61
flamingo1324 <ksw29555@gmail.com>
2024-02-22 13:26:57 +0900

    helm provider fix

--- terraform/modules/eks/eks_bootstrap.tf
+++ terraform/modules/eks/eks_bootstrap.tf
@@ -15,6 +15,31 @@ locals {
   argocd_vault_plugin_version = "1.17.0"
 }
 
+data "aws_eks_cluster" "cluster" { name = module.eks.cluster_name }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.id]
+      command     = "aws"
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.id]
+    command     = "aws"
+  }
+}
+
+
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
