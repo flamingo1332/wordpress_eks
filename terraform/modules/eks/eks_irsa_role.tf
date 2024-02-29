513d5c7e0dcadf86abfb831e1c5861388a071a6d
flamingo1324 <ksw29555@gmail.com>
2024-02-29 19:58:46 +0900

    fix

--- terraform/modules/eks/eks_irsa_role.tf
+++ terraform/modules/eks/eks_irsa_role.tf
@@ -4,13 +4,13 @@
 
 #cluster_autoscaler
 module "cluster_autoscaler_irsa_role" {
-  create_role = true
-  source      = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version     = "~> 5.0"
+  create_role                      = true
+  source                           = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version                          = "~> 5.0"
   role_name                        = "cluster-autoscaler"
   attach_cluster_autoscaler_policy = true
-  cluster_autoscaler_cluster_ids = [module.eks.cluster_name]
-  
+  cluster_autoscaler_cluster_ids   = [module.eks.cluster_name]
+
   oidc_providers = {
     one = {
       provider_arn               = module.eks.oidc_provider_arn
@@ -26,11 +26,11 @@ module "cluster_autoscaler_irsa_role" {
 
 # externalDNS
 module "external_dns_irsa_role" {
-  create_role = true
-  source      = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version     = "~> 5.0"
-  role_name                  = "external-dns"
-  attach_external_dns_policy = true
+  create_role                   = true
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version                       = "~> 5.0"
+  role_name                     = "external-dns"
+  attach_external_dns_policy    = true
   external_dns_hosted_zone_arns = [var.hosted_zone_arn]
 
   oidc_providers = {
